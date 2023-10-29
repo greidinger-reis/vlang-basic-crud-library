bfe92826f563769930eb2832ca70606c80f1d590
greidinger-reis <jp.greidinger@hotmail.com>
2023-10-29 11:13:57 -0300

    sidemenu

--- src/admin_view.v
+++ src/admin_view.v
@@ -3,7 +3,7 @@ module main
 import vweb
 
 ['/admin/books'; get]
-pub fn (mut ctx App) admin_booklist() vweb.Result {
+pub fn (mut ctx App) admin_books() vweb.Result {
 	page_title := 'Admin Book Page'
 	book_list := ctx.book_find_all()
 	csrf_token := ctx.get_csrf_token()
@@ -11,10 +11,16 @@ pub fn (mut ctx App) admin_booklist() vweb.Result {
 	return $vweb.html()
 }
 
-['/admin/books/new'; get]
-pub fn (mut ctx App) admin_newbook() vweb.Result {
-	page_title := 'Admin New Book Page'
+['/admin'; get]
+pub fn (mut ctx App) admin_index() vweb.Result {
+	page_title := 'Admin Page'
 	csrf_token := ctx.get_csrf_token()
 
+	return $vweb.html()
+}
+
+['/admin/sidemenu'; get]
+pub fn (mut ctx App) admin_sidemenu() vweb.Result {
+	location := ctx.req.header.get_custom('Location') or { 'unknown' }
 	return $vweb.html()
 }
