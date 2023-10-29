23bc03718fdaf99b0830e83ca193403706c25963
greidinger-reis <jp.greidinger@hotmail.com>
2023-10-29 16:28:40 -0300

    table navigation ok

--- src/admin_view.v
+++ src/admin_view.v
@@ -6,6 +6,8 @@ import math
 ['/admin/books'; get]
 pub fn (mut ctx App) admin_books() vweb.Result {
 	page_title := 'Admin Book Page'
+	csrf_token := ctx.get_csrf_token()
+
 	mut current_page := ctx.query['page'].int()
 	if current_page == 0 {
 		current_page = 1
@@ -15,9 +17,33 @@ pub fn (mut ctx App) admin_books() vweb.Result {
 	offset := (current_page - 1) * 10
 
 	book_list, book_full_len := ctx.book_find_all(limit, offset)
-	csrf_token := ctx.get_csrf_token()
 
-	book_pagination_number := math.ceil(f64(book_full_len) / 10.0)
+	page_count := math.ceil(f64(book_full_len) / 10.0)
+
+	return $vweb.html()
+}
+
+['/admin/books/partial'; get]
+pub fn (mut ctx App) admin_partialbooks() vweb.Result {
+	mut current_page := ctx.query['page'].int()
+	if current_page == 0 {
+		current_page = 1
+	}
+
+	limit := current_page * 10
+	offset := (current_page - 1) * 10
+
+	book_list, book_full_len := ctx.book_find_all(limit, offset)
+
+	page_count := math.ceil(f64(book_full_len) / 10.0)
+
+	return $vweb.html()
+}
+
+['/admin/books/partial/navigation'; get]
+pub fn (mut ctx App) admin_partialnavigation() vweb.Result {
+	page_count := ctx.req.header.get_custom('page_count') or { '1' }.int()
+	current_page := ctx.req.header.get_custom('current_page') or { '1' }.int()
 
 	return $vweb.html()
 }
